2b68a3499513eb3fc712b42a9bb75e7e64c97580
Omar <omarsherifmohammedomar@gmail.com>
2024-12-16 22:12:17 +0200

    .

--- Headers/Game.h
+++ Headers/Game.h
@@ -8,12 +8,15 @@ private:
     Player *p1;
     Player *p2;
     Deck *d;
+    Player* currentPlayer;
+
 
 public:
     Game() {
         p2 = new Player("Player 2", 0);
         p1 = new Player("Player 1", 0);
         d = new Deck();
+
     }
 
     ~Game() {
@@ -22,6 +25,25 @@ public:
         delete d;
     }
 
-
+void switchTurn();
+    void handleCardPair(Card* card1, Card* card2);
+    void handleStandardCards(Card* card1, Card* card2);
+    void handleBonusCards(Card* card1, Card* card2);
+    void handlePenaltyCards(Card* card1, Card* card2);
     void initializeGame();
+
+    void playTurn(Player *player);
+
+    void handleBonusCard(Player *player, Card *card1, Card *card2, int x1, int y1, int x2, int y2);
+
+    void handlePenaltyCard(Player *player, Card *card1, Card *card2, int x1, int y1, int x2, int y2);
+
+    void startGame();
+
+    bool isGameOver();
+
+    void announceWinner();
+
+    void playGame();
+    void announceWinner() const;
 };
